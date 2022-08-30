6d508d59269a10605088391e71837187ed9bfadd
Ryan McGowan <vitomaserati@gmail.com>
2022-08-29 20:22:35 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
+function App(){
+    let [ counter, changeCounter ] = useState(1)
+    window.changeCounter = changeCounter
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <h4>{counter}</h4>
+        </div>
+    )
+}
+
+export default App;
+
+
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -20,6 +37,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
-export default App;
+
